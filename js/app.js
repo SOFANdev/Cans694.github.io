862fcae5e8e0f83a46f27e4b51d69b3b2a404cb7
SOFANdev <82921207+SOFANdev@users.noreply.github.com>
2022-07-30 20:30:57 +0545

    My personal website

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Welcome to my website.", "FUN", "Enjoy."],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true
+});
+
+// const cursor = document.querySelector('.cursor');
+// document.addEventListener('mousemove' , e =>{
+//     cursor.setAttribute("style" , "top: "+(e.pageY - 20)+"px; left: "+(e.pageX - 20)+"px;")
+// });
+
+// document.addEventListener('click', () => {
+//     cursor.classList.add("expand");
+
+//     setTimeout(() => {
+//         cursor.classList.remove("expand");
+//     }, 300);
+// })
