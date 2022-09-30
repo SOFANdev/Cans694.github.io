97d4968deffcbe60bd107911258de337d4aa4f43
SOFANdev <82921207+so-fan@users.noreply.github.com>
2022-09-30 19:14:48 +0545

    Cans694

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var typed = new Typed(".auto-type", {
-    strings: ["Welcome to my website.", "Enjoy."],
-    typeSpeed: 150,
-    backSpeed: 50,
-    loop: true
+    strings: ["Welcome."],
+    typeSpeed: 150
+    // backSpeed: 50,
+    // loop: true
 });
 
 // const cursor = document.querySelector('.cursor');
